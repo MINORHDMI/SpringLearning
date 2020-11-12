55c14b61168c84464d06f2b63cc804ed52309464
MINORHDMI <1019364668@qq.com>
2020-11-12 15:57:43 +0800

    Initial Commit

---
+++ src/com/tutorialspoint/MainApp.java
@@ -0,0 +1,23 @@
+package com.tutorialspoint;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+        // 使用ApplicationContext 上下文
+        // ApplicationContext context =
+        //         new ClassPathXmlApplicationContext("Beans.xml");
+        //HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+
+        //使用XmlBeanFactory
+        XmlBeanFactory factory = new XmlBeanFactory
+                (new ClassPathResource("Beans.xml"));
+        HelloWorld obj = (HelloWorld) factory.getBean("helloWorld");
+        obj.getMessage();
+    }
+}
