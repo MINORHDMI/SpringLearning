222e9cdefdba4cee27492acba5709237948356e9
minor <1019364668@qq.com>
2020-11-15 13:58:24 +0800

    新增

--- src/com/tutorialspoint/MainApp.java
+++ src/com/tutorialspoint/MainApp.java
@@ -1,5 +1,6 @@
 package com.tutorialspoint;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import org.springframework.beans.factory.InitializingBean;
@@ -9,15 +10,17 @@ import org.springframework.core.io.ClassPathResource;
 public class MainApp {
     public static void main(String[] args) {
 
-        // 使用ApplicationContext 上下文
-        // ApplicationContext context =
-        //         new ClassPathXmlApplicationContext("Beans.xml");
-        //HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+         // 使用ApplicationContext 上下文
+         AbstractApplicationContext context =
+                 new ClassPathXmlApplicationContext("Beans.xml");
+         HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+         obj.getMessage();
+         context.registerShutdownHook();
 
-        //使用XmlBeanFactory
-        XmlBeanFactory factory = new XmlBeanFactory
-                (new ClassPathResource("Beans.xml"));
-        HelloWorld obj = (HelloWorld) factory.getBean("helloWorld");
-        obj.getMessage();
+//        使用XmlBeanFactory
+//        XmlBeanFactory factory = new XmlBeanFactory
+//                (new ClassPathResource("Beans.xml"));
+//        HelloWorld obj = (HelloWorld) factory.getBean("helloWorld");
+//        obj.getMessage();
     }
 }
